fd7bcc89288fac9de45728800b37292e3c34ac38
Kaeri Silva Paredes <101607731+KaeriSilva@users.noreply.github.com>
2023-09-18 15:58:24 -0600

    Add files via upload
    Interfaz mediante la terminal como plan b
    Agreguen sus funciones a la interfaz, cada caso viene comentado para que sepan donde agregar sus funciones creadas

---
+++ interfaz_terminal.py
@@ -0,0 +1,40 @@
+from colorama import Back, Fore, Style
+from funcion_registro import registro_usuario
+print(Back.BLACK + Fore.BLUE + Style.BRIGHT + """
+      *-------------------*
+      | S I M U L A D O R |
+      | B A N C A R I O   |
+      *-------------------*
+      1. Registro Usuario
+      2. Consultar saldo
+      3. Retiro
+      4. Deposito
+      5. Transferencia
+      6. Historial de transacciones
+      7. Salir
+      """)
+opcion = int(input("Ingresa una opcion: "))
+
+match opcion:
+    
+    case 1:
+        nombreCuenta = input("Ingresa tu nombre completo: ")
+        if registro_usuario(nombreCuenta,saldo):
+            print("Registro de usuario exitosamente!")
+        else:
+            print("Usuario ya existente")
+    case 2:
+        #Consuta saldo
+    case 3:
+        #Retiro
+    case 4:
+        #Deposito
+    case 5: 
+        #Tansferencia
+    case 6:
+        #Historial de transacciones 
+    case 7:
+        print("Saliendo...")
+        print("Vuelva pronto :)")   
+    case _: 
+        print("Opcion no valida")     
