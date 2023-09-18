892a4527ef24f96278ad9cfb88b2f15cf4d15d73
dlucianop <3112dlp@gmail.com>
2023-09-18 15:21:01 -0600

    Funcion transferencias, el saldo y monto son de tipo FLOAT ahora

--- funcion_transferencias.py
+++ funcion_transferencias.py
@@ -13,21 +13,21 @@ def transferencias(id_origen, id_destino, monto):
     "FROM Cuentas "
     "WHERE ID = %s ;",(id_origen))
 
-    if saldoactual < monto:
+    if float(saldoactual) < float(monto):
         print("No cuenta con el saldo suficiente para realizar esta operación")
     else:
         #query para transacciones
         cursor.execute("INSERT INTO Transacciones (IDCuenta, Tipo, Monto, Fecha) "
-        " VALUES (%s, %s, %s, %s) ;",(id_origen, "Transferencia", monto, date_now))
+        " VALUES (%s, %s, %s, %s) ;",(int(id_origen), "Transferencia", float(monto), date_now))
 
         #monto restado
         cursor.execute("UPDATE Cuentas "
         "SET Saldo = Saldo - %s "
-        "WHERE ID = %s ;", (monto, id_origen))
+        "WHERE ID = %s ;", (float(monto), id_origen))
 
         #monto agregado
         cursor.execute("UPDATE Cuentas "
         "SET Saldo = Saldo + %s "
-        "WHERE ID = %s ;", (monto, id_destino))
+        "WHERE ID = %s ;", (float(monto), id_destino))
 
     cursor.close()
